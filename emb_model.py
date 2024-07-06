273305ceb3f5066340c169603756a556c95a4792
Manasi Chhibber <68627617+Manasi2001@users.noreply.github.com>
2024-07-07 00:00:36 +0300

    Update emb_model.py

--- emb_model.py
+++ emb_model.py
@@ -1,20 +1,23 @@
+"""
+A deep neural network architecture for probabilistic detectors.
+
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class emb_fully_1(nn.Module):
-    """
-    A deep neural network architecture for spoken language recognition 
-    """
     def __init__(self, idim, hdim, odim):
         super(emb_fully_1, self).__init__()
         self.layers = nn.ModuleList()
         cdim=idim
-        #frame laval information capture using fc layers
-        if hdim!= []:
-            for dim in hdim:
-                self.layers.append(nn.Linear(cdim, dim))
-                cdim=dim
+
+        # adding hidden layers
+        for dim in hdim:
+            self.layers.append(nn.Linear(cdim, dim))
+            cdim=dim
         self.layers.append(nn.Linear(cdim, odim))
     
         
@@ -24,12 +27,11 @@ class emb_fully_1(nn.Module):
                 x= F.relu(layer(x))
         x=self.layers[-1](x)
         return  x 
-    
 
     
 def initialize_weights(model_layer):
     if type(model_layer) == nn.Linear:
         torch.nn.init.xavier_normal_(model_layer.weight)
-        #print(model_layer.weight)
+        # print(model_layer.weight)
     if type(model_layer) == nn.Conv1d:
         torch.nn.init.normal_(model_layer.weight, mean=0.0, std=0.9)
